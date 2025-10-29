d37f35ca1d3177c80e4cc590bc9dedb3784bb0f4
Nico Steven101 <castroconi101@gmail.com>
2025-10-30 02:00:54 +0800

    Updated Home Page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -18,23 +18,23 @@ export default function Home(){
         </div>
         </div>
         <div id="centerPart" className="">
- <div id="helloWorldCard" className="w-[40vw] ml-4 mt-[10vh] bg-white border border-black rounded-4xl p-8"
+ <div id="helloWorldCard" className="w-[40vw] ml-4 mt-[19vh] bg-white border border-black rounded-4xl p-8"
 >
   <h1 className="text-[5vw]">
     Hello,<span className="italic">World!</span>
   </h1>
-  <p className="text-[2.8vh] leading-relaxed text-gray-800">
+  <p className="text-[3vh] leading-relaxed text-black">
     How are ya? I’m a creative who loves problem-solving and thinking-critically.
     Man.. living in this world is hard, so let’s make it s easier together!
   </p>
 </div>
-        <div className="flex">
+<div id="centerPartBottom" className="flex">
 <div className="flex flex-col items-start space-y-3 mt-4" id="moreButtons">
-  <p className="text-gray-700 text-lg">To know more,</p>
-  <button className="bg-black text-white font-medium py-2 px-6 rounded-full hover:bg-white hover:text-black transition duration-300">
+  <p className="text-black text-lg">To know more,</p>
+  <button className="bg-black text-2xl text-white py-2 px-6 rounded-full w-56 h-16">
     Recent Projects
   </button>
-  <button className="bg-black text-white font-medium py-2 px-6 rounded-full hover:bg-gray-800 transition duration-300">
+  <button className="bg-black text-2xl text-white py-2 px-6 rounded-full w-56 h-16">
     Let’s Talk!
   </button>
 </div>
@@ -43,23 +43,34 @@ export default function Home(){
   <img
     src="/earth.gif"
     alt="Earth"
-    className="absolute top-[35vh] right-[27vw] w-[50vw] h-[50vh] object-contain"
+    className="absolute top-[48vh] right-[24vw] w-[50vw] h-[50vh] object-contain"
   />
         </div>
         </div>
-            </section>
+</section>
             
-            <section id="rightSide">
-                <div id="introAboutMe">
-                    <img>
-                    </img>
-                    <p>
-                       Welcome! I’m Nico Castro, 
-A Software/Web Engineer | System Architect | Graphic Designer | Tech Instructor |Tech Leader | 
-                    </p>
-                </div>
-                {/* Place the External Link Components here */}
-            </section>
+          <section
+  id="rightSide"
+  className="flex justify-end w-full"
+>
+  <div
+    id="introAboutMe"
+    className="blackBox bg-black text-white rounded-l-4xl mt-12 p-4 flex flex-col max-w-sm"
+  >
+    <img
+      className="relative right-32 w-[45vw] h-[40vh] self-end rounded-md mb-2"
+      src="damn.jpg"
+      alt="Profile"
+    />
+    <p className="leading-relaxed text-xl">
+      <span className="italic font-semibold">Welcome!</span> I’m Nico Castro, <br />
+      A Software/Web Engineer | System Architect | Graphic Designer | Tech
+      Instructor | Tech Leader |
+    </p>
+  </div>
+  {/* Place the External Link Components here */}
+</section>
+
         </div>
     )
 }
