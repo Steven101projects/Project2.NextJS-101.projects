42f61584271ea4cc66cbe64113ae81f9ae9b02bc
Nico Steven101 <castroconi101@gmail.com>
2025-11-03 23:47:37 +0800

    Updated Home and Projects Pages

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,85 +1,94 @@
-import Link from 'next/link';
-import NavWheel from '../../components/NavWheel';
-import ExternalLinks from '../../components/ExternalLinks';
+import Link from "next/link";
+import NavWheel from "../../components/NavWheel";
+import ExternalLinks from "../../components/ExternalLinks";
+import Image from "next/image";
 
-export default function Home(){
-
-    return (
-        <div className="flex">
-        <section id="leftSide" className="flex">
+export default function Home() {
+  return (
+    <div className="flex">
+      <section id="leftSide" className="flex">
         <div id="links">
-<Link href="/" className="px-2 py-1 text-8xl">
-        1O1~
-        </Link>
-        <nav>
-                <NavWheel />
-            </nav>
-        <div id="copyRight" className="text-[1.4rem] ml-3.5 mt-2.5">
-            Copyright © 2025 <br></br> Nico Castro.  <br></br> All rights reserved.
+          <Link href="/" className="px-2 py-1 text-8xl">
+            1O1~
+          </Link>
+          <nav>
+            <NavWheel />
+          </nav>
+          <div id="copyRight" className="text-[1.4rem] ml-3.5 mt-2.5">
+            Copyright © 2025 <br /> Nico Castro. <br /> All rights reserved.
+          </div>
         </div>
-        </div>
+
         <div id="centerPart" className="">
-<div
-  id="helloWorldCard"
-  className="right-[2vw] w-[50vw] h-[50vh] ml-3 mt-[5vh] bg-white border border-black rounded-4xl pl-6"
->
-  <h1 className="text-[7.4vw] text-[#222222]">
-    Hello,<span className="italic">World!</span>
-  </h1>
-<p className="relative bottom-5 text-[3.9vh] text-[#222222] ml-2 italic">
-  Hey there! I’m a creative and tech enthusiast who loves solving problems, thinking critically, 
-  and bringing ideas to life through innovation. Life can be challenging at times, <br /> 
-  so let’s make it easier together!
-</p>
-</div>
-<div id="centerPartBottom" className="flex">
-<div className="flex flex-col items-start space-y-3 mt-4" id="moreButtons">
-  <p className="text-black text-2xl">To know more,</p>
+          <div
+            id="helloWorldCard"
+            className="right-[2vw] w-[50vw] h-[50vh] ml-3 mt-[5vh] bg-white border border-black rounded-4xl pl-6"
+          >
+            <h1 className="text-[7.4vw] text-[#222222]">
+              Hello,<span className="italic">World!</span>
+            </h1>
+            <p className="relative bottom-5 text-[3.9vh] text-[#222222] ml-2 italic">
+              Hey there! I’m a creative and tech enthusiast who loves solving
+              problems, thinking critically, and bringing ideas to life through
+              innovation. Life can be challenging at times, <br />
+              so let’s make it easier together!
+            </p>
+          </div>
 
-  <button className="relative hover:font-mono overflow-hidden hover:border-2 hover:border-black bg-black text-2xl text-white py-2 px-6 rounded-full w-56 h-20 transform transition duration-500 hover:scale-110 hover:bg-gradient-to-r hover:from-gray-300 hover:via-white hover:to-gray-300 hover:text-black shadow-lg">
-    <span className="relative z-10">Recent Projects</span>
-    <span className="absolute inset-0 bg-gradient-to-r from-transparent via-white/40 to-transparent opacity-0 hover:opacity-100 transition-opacity duration-700"></span>
-  </button>
+          <div id="centerPartBottom" className="flex">
+            <div
+              className="flex flex-col items-start space-y-3 mt-4"
+              id="moreButtons"
+            >
+              <p className="text-black text-2xl">To know more,</p>
 
-  <button className="relative hover:font-mono overflow-hidden hover:border-2 hover:border-black bg-black text-2xl text-white py-2 px-6 rounded-full w-56 h-20 transform transition duration-500 hover:scale-110 hover:bg-gradient-to-r hover:from-gray-300 hover:via-white hover:to-gray-300 hover:text-black shadow-lg">
-    <span className="relative z-10">Let’s Talk!</span>
-    <span className="absolute inset-0 bg-gradient-to-r from-transparent via-white/40 to-transparent opacity-0 hover:opacity-100 transition-opacity duration-700"></span>
-  </button>
-</div>
+              <button className="relative hover:font-mono overflow-hidden hover:border-2 hover:border-black bg-black text-2xl text-white py-2 px-6 rounded-full w-56 h-20 transform transition duration-500 hover:scale-110 hover:bg-gradient-to-r hover:from-gray-300 hover:via-white hover:to-gray-300 hover:text-black shadow-lg">
+                <span className="relative z-10">Recent Projects</span>
+                <span className="absolute inset-0 bg-gradient-to-r from-transparent via-white/40 to-transparent opacity-0 hover:opacity-100 transition-opacity duration-700"></span>
+              </button>
 
-  {/* Rotating Globe Img */}
-  <img
-    src="/earth.gif"
-    alt="Earth"
-    className="absolute top-[38vh] right-[11vw] w-[60vw] h-[60vh] object-contain"
-  />
+              <button className="relative hover:font-mono overflow-hidden hover:border-2 hover:border-black bg-black text-2xl text-white py-2 px-6 rounded-full w-56 h-20 transform transition duration-500 hover:scale-110 hover:bg-gradient-to-r hover:from-gray-300 hover:via-white hover:to-gray-300 hover:text-black shadow-lg">
+                <span className="relative z-10">Let’s Talk!</span>
+                <span className="absolute inset-0 bg-gradient-to-r from-transparent via-white/40 to-transparent opacity-0 hover:opacity-100 transition-opacity duration-700"></span>
+              </button>
+            </div>
+
+            {/* Rotating Globe Img */}
+            <div className="absolute top-[38vh] right-[11vw] w-[60vw] h-[60vh] object-contain">
+              <Image
+                src="/earth.gif"
+                alt="Earth"
+                fill
+                className="object-contain"
+              />
+            </div>
+          </div>
         </div>
-        </div>
-</section>
-            
-          <section
-  id="rightSide"
-  className="flex justify-end w-full"
->
-  <div
-    id="introAboutMe"
-    className="blackBox bg-black text-white rounded-l-4xl mt-10 p-4 flex flex-col max-sm:"
-  >
-<img
-  className="absolute right-5 w-[28vw] h-[50vh] object-cover rounded-4xl "
-  src="damn.jpg"
-  alt="Profile"
-/>
-    <p className="leading-relaxed text-[3.4vh] p-3  mt-[50vh]">
-      <span className="italic font-semibold">Welcome!</span> I’m Nico Castro, <br />
-      A Software/Web Engineer | <br /> Software System Architect |  Graphic Designer | Tech
-      Instructor | Tech Leader |
-    </p>
-  </div>
-  {/* Place the External Link Components here */}
-  <ExternalLinks />
-</section>
+      </section>
 
+      <section id="rightSide" className="flex justify-end w-full">
+        <div
+          id="introAboutMe"
+          className="blackBox bg-black text-white rounded-l-4xl mt-10 p-4 flex flex-col relative"
+        >
+          <div className="absolute right-5 w-[28vw] h-[50vh]">
+            <Image
+              src="/damn.jpg"
+              alt="Profile"
+              fill
+              className="object-cover rounded-4xl"
+            />
+          </div>
+          <p className="leading-relaxed text-[3.4vh] p-3 mt-[50vh]">
+            <span className="italic font-semibold">Welcome!</span> I’m Nico
+            Castro, <br />
+            A Software/Web Engineer | <br /> Software System Architect | Graphic
+            Designer | Tech Instructor | Tech Leader |
+          </p>
         </div>
-    )
+        {/* External Links Component */}
+        <ExternalLinks />
+      </section>
+    </div>
+  );
 }
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,47 +1,61 @@
-"use client"
+"use client";
 
 import ProjectHeader from "@/components/projects-components/ProjectsHeader";
-import { ViewAllProjects, RecentProjects } from "@/components/projects-components/ViewProjects";
+import {ViewAllProjects, RecentProjects,} from "@/components/projects-components/ViewProjects";
 import { useState } from "react";
+import Image from "next/image";
 
+/* eslint-disable react/no-unescaped-entities */
 export default function Projects() {
-  // Array of quotes
   const quotes = [
-    { text: "Every brushstroke is a step toward mastery.", author: "Nikola Tesla" },
+    {
+      text: "Every brushstroke is a step toward mastery.",
+      author: "Nikola Tesla",
+    },
     { text: "Creativity takes courage.", author: "Henri Matisse" },
-    { text: "Art enables us to find ourselves and lose ourselves at the same time.", author: "Thomas Merton" },
+    {
+      text: "Art enables us to find ourselves and lose ourselves at the same time.",
+      author: "Thomas Merton",
+    },
     { text: "Simplicity is the ultimate sophistication.", author: "Leonardo da Vinci" },
   ];
 
-    const [showAll, setShowAll] = useState(false);
+  const [showAll, setShowAll] = useState(false);
 
-  // Pick a random quote each time the component loads
   const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
   return (
     <div>
       <ProjectHeader />
-      <div id="quoteBlock" className="relative flex justify-center items-center mt-6">
+      <div
+        id="quoteBlock"
+        className="relative flex justify-center items-center mt-6"
+      >
         {/* Image */}
-        <img
-          src="quotes.jpg"
-          className="w-[55vw] h-[45vh] object-cover shadow-md rounded-md"
-          alt="Quote Block"
-        />
+        <div className="w-[55vw] h-[45vh] relative shadow-md rounded-md overflow-hidden">
+          <Image
+            src="/quotes.jpg"
+            alt="Quote Block"
+            fill
+            className="object-cover"
+            priority
+          />
+        </div>
 
-        {/* Centered quote text overlay */}
+        {/* Quote text overlay */}
         <div className="absolute text-center text-white px-6">
           <p className="text-[5vh] italic mb-2 drop-shadow-lg">
-            "{randomQuote.text}"
+            &quot;{randomQuote.text}&quot;
           </p>
           <p className="text-[3vh] font-light drop-shadow-md">
-            - {randomQuote.author}
+            &ndash; {randomQuote.author}
           </p>
         </div>
       </div>
+
       <div id="projects" className="mt-10">
         {showAll ? (
-          <ViewAllProjects  setShowAll={setShowAll} />
+          <ViewAllProjects setShowAll={setShowAll} />
         ) : (
           <RecentProjects setShowAll={setShowAll} />
         )}
