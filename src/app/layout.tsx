8a7b2153dbdbf6cf6418de9a4d20a3ec3235661c
Nico Steven101 <castroconi101@gmail.com>
2025-11-30 17:15:59 -0500

    another one?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "WELCOME | 101.PROJECTS",
+  title: "101.Projects",
   description: "Portfolio by Nico Castro",
   icons: {
     icon: [
